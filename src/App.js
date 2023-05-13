b54d8edc48361eb568ee0e4291f5da4e4889a5d2
Vanshika Dargan <101962910+Vanshika-Dargan@users.noreply.github.com>
2023-05-13 21:48:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Clue4 from "./components/Clues/Clue4/Clue4.js";
 import Clue5 from "./components/Clues/Clue5/Clue5.js";
 import DeadEnd1 from "./components/DeadEnd/DeadEnd1/DeadEnd1.js"
 import Dashboard from "./components/Dashboard/Dashboard.js";
-
+import Final from "./components/Clues/Final/Final.js"
 const App = () => {
 
   const [finished,setFinished]=useState(false);
@@ -32,28 +32,30 @@ const App = () => {
 });
   const user=JSON.parse(localStorage.getItem('profile'));
   return (
-   <GoogleOAuthProvider clientId="1054788579196-s0dj3mtah60b5vl7c7he9otklr7tpfpn.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId="1054788579196-s0dj3mtah60b5vl7c7he9otklr7tpfpn.apps.googleusercontent.com">
     <BrowserRouter>
     <ThemeProvider theme={theme}>
     <Container maxWidth="xl" sx={{backgroundColor:'#2A2F4F'}}>
       
       <Routes>
-       <Route path="/" element={<Navigate to="/home"/>}/>
         <Route path="/home" element={<Navbar finished={finished} setFinished={setFinished}/>}/>
+        <Route path="/" element={<Navigate to="/home"/>}/>
+        <Route path="/home" element={<Home/>}/>
         <Route path="/deadend1" element={<DeadEnd1/>}/>
-        <Route  path="/dashboard" element={user?.result?.email==='admin@gmail.com'&&<Dashboard/>}/>
-        <Route path="/clue1" element={user && <Clue1 />}/>
-        <Route path="/clue2" element={user && <Clue2 />}/>
-        <Route path="/clue3" element={user && <Clue3 />}/>
-        <Route path="/clue4" element={user && <Clue4 />}/>
-        <Route path="/clue5" element={user && <Clue5 setFinished={setFinished}/>}/>
-        <Route path="/auth" element={!user ? <Auth /> : <Navigate to="/home" /> }/>
+        <Route  path="/dashboard" element={<Dashboard/>}/>
+        <Route key={window.location.pathname} path="/clue1" element={<Clue1 />}/>
+        <Route path="/clue2" element={<Clue2 />}/>
+        <Route path="/clue3" element={<Clue3 />}/>
+        <Route path="/clue4" element={<Clue4 />}/>
+        <Route path="/clue5" element={<Clue5 setFinished={setFinished}/>}/>
+        <Route path="/final" element={<Final/>}/>
+        <Route path="/auth" element={<Auth/>}/>
       </Routes>
-     
+      
     </Container>
     </ThemeProvider>
     </BrowserRouter>
-     </GoogleOAuthProvider>
+    </GoogleOAuthProvider>
   );
 };
 export default App;
